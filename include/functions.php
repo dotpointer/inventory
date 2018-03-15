cba57d26716d0ad0203e5bd7c771356516669718
Robert Klebe <dotpointer@gmail.com>
2018-03-15 02:48:54 +0100

    Removing debug code.

--- include/functions.php
+++ include/functions.php
@@ -343,14 +343,6 @@
 			}
 		}
 
-$file = '/tmp/people.txt';
-// Open the file to get existing content
-$current = file_get_contents($file);
-// Append a new person to the file
-$current .= $s."\n";
-// Write the contents back to the file
-file_put_contents($file, $current);
-
 		return $s;
 	}
 
